f11246b720d8d3a8f0c7e0037954da2cd5281814
leahss <59526763+leahss@users.noreply.github.com>
2025-07-20 13:55:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 // Change section visibility based on project type
 document.getElementById("projectType").addEventListener("change", function () {
-    const type = this.value;
-    document.querySelectorAll(".conditional").forEach(section => section.style.display = "none");
-    if (type === "Spare Parts") {
-      document.getElementById("sparePartsSection").style.display = "block";
-    } else if (type === "Field Service") {
-      document.getElementById("fieldServiceSection").style.display = "block";
-    } else if (type === "Large Project") {
-      document.getElementById("largeProjectSection").style.display = "block";
-    }
-  });
-  
-  // Handle form submission
-  //https://script.google.com/macros/s/AKfycbw4q3CTd62OVXN6JguBhLvhYvTykEDW_6GhTTufnAsM4qqnVage31sawKuyDjqhUYi4/exec
+  const type = this.value;
+  document.querySelectorAll(".conditional").forEach(section => section.style.display = "none");
+  if (type === "Spare Parts") {
+    document.getElementById("sparePartsSection").style.display = "block";
+  } else if (type === "Field Service") {
+    document.getElementById("fieldServiceSection").style.display = "block";
+  } else if (type === "Large Project") {
+    document.getElementById("largeProjectSection").style.display = "block";
+  }
+});
 
-  document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("survey-form");
 
   form.addEventListener("submit", async function (e) {
